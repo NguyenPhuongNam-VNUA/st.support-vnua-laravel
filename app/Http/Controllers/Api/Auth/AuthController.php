4b7d56dd7735c7d94ac5b764d49f8c35b35861a8
Nguyễn Phương Nam <namdev@MacBook-Pro-cua-Nguyen.local>
2025-07-14 22:40:58 +0700

    add login

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class AuthController extends Controller
+{
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Email hoặc mật khẩu không đúng'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('auth_token', [], now()->addHours(2))->plainTextToken;
+
+        return response()->json([
+            'message' => 'Đăng nhập thành công',
+            'token' => $token,
+            'user' => $user
+        ], 200);
+    }
+
+    public function user(Request $request): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'message' => 'Thông tin người dùng',
+            'user' => $request->user()
+        ], 200);
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Đăng xuất thành công'
+        ], 200);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin_st@gmail.com',
+            'password' => bcrypt('123456aA@'), // Use bcrypt for password hashing
         ]);
     }
 }
